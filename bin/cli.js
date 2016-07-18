a92f8051d797118d36e9e4068ecf6e13c0e07666
Jonathan Knapp <jon@coffeeandcode.com>
2016-07-18 06:44:17 -0400

    Move remaining cli commands to their own modules

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
-const { writeFileSync } = require('fs');
-const mkdirp = require('mkdirp');
-const minimist = require('minimist');
-const { join } = require('path');
-const rimraf = require('rimraf');
+const build = require('../lib/cli/build');
+const compress = require('../lib/cli/compress');
 const help = require('../lib/cli/help');
+const minimist = require('minimist');
 const version = require('../lib/cli/version');
+const watch = require('../lib/cli/watch');
 
 const command = minimist(process.argv.slice(2), {
   alias: {
@@ -28,30 +26,12 @@ if (command.help) {
 }
 
 if (process.argv.length <= 2 || command.build) {
-  const config = require('../lib/config');
-
-  rimraf.sync(join(process.cwd(), config.output));
-  mkdirp.sync(join(process.cwd(), config.output));
-
-  spawn(join(__dirname, 'build-css.js'), { stdio: 'inherit' });
-  spawn(join(__dirname, 'build-html.js'), { stdio: 'inherit' });
-  spawn(join(__dirname, 'build-js.js'), { stdio: 'inherit' });
-
+  build();
+  if (command.compress) {
+    compress();
+  }
 } else if (command.compress) {
-  spawn(join(__dirname, 'gzip.js'), { stdio: 'inherit' });
-
+  compress();
 } else if (command.watch) {
-  const config = require('../lib/config');
-
-  rimraf.sync(join(process.cwd(), config.output));
-  mkdirp.sync(join(process.cwd(), config.output));
-
-  writeFileSync('Procfile', `# Procfile created by Static
-css: ${require.resolve('nodemon/bin/nodemon')} --ext css --exec "${join(__dirname, 'build-css.js')}" --watch ${config.folders.css}
-html: ${require.resolve('nodemon/bin/nodemon')} --ext hbs --exec "${join(__dirname, 'build-html.js')}" --watch ${config.folders.pages} --watch ${config.folders.views}
-js: ${require.resolve('nodemon/bin/nodemon')} --exec "${join(__dirname, 'build-js.js')}" --watch ${config.folders.javascript}
-server: ${require.resolve('browser-sync/bin/browser-sync')} start --config ${join(__dirname, '../config/browsersync.js')}
-`);
-
-  spawn(require.resolve('foreman/nf'), ['start'], { stdio: 'inherit' });
+  watch();
 }
