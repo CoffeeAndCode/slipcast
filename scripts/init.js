546078f645ecf6a13f0b81a92bb7f7b5b264f1a3
Jonathan Knapp <jon@coffeeandcode.com>
2016-07-23 15:23:26 -0400

    Update package.json when init'ing new project

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { execSync } = require('child_process');
+const { spawn } = require('child_process');
+const { readFile, writeFile } = require('fs');
 const { join } = require('path');
 
 module.exports = function(projectDirectory, appName, verbose) {
@@ -8,6 +9,17 @@ module.exports = function(projectDirectory, appName, verbose) {
     console.log(`Creating project for ${appName} in ${projectDirectory}...`);
   }
 
-  // add `scripts` to package.json
-  execSync(`cp ${verbose ? '-v' : ''} -R ${join(__dirname, '../template/')} ${projectDirectory}`, { stdio: 'inherit' });
+  console.log('Updating package.json scripts');
+  const packageJSON = require(join(parentDirectory, 'package.json'));
+  packageJSON.scripts = {
+    build: 'slipcast --build',
+    compress: 'slipcast --compress',
+    start: 'slipcast --watch'
+  };
+  writeFile(join(parentDirectory, 'package.json'), JSON.stringify(packageJSON, null, 2), function(error, data) {
+    if (error) { throw error; }
+  });
+
+  console.log('Copying template into your project');
+  spawn('cp', [verbose ? '-v' : '', '-R', join(__dirname, '../template/'), projectDirectory], { stdio: 'inherit' });
 }
